8ed3db85987b54cbed86a26562bad6d7d3cfd680
snailvoyager <kyj88514@gmail.com>
2025-07-13 23:46:49 +0900

    redis init

---
+++ src/main/java/com/snailvoyager/HelloController.java
@@ -0,0 +1,34 @@
+package com.snailvoyager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "saved." + name;
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        return ops.get("fruit");
+    }
+}
